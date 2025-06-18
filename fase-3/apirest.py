79fbfbd0ed9ec642ecca52b56438d5b63445c1ea
Miguel Puerta <110676608+MiguelAPuerta@users.noreply.github.com>
2025-06-18 06:29:40 -0500

    Final apirest update

--- fase-3/apirest.py
+++ fase-3/apirest.py
@@ -4,10 +4,8 @@ from predict import predict_model
 
 app = Flask(__name__)
 
-global train_status
-global predict_status
-train_status = "not training"
 predict_status = "not predicting"
+train_status = "not training"
     
 @app.route("/")
 def hello_world():
@@ -22,9 +20,11 @@ def status():
 
 @app.route("/predict", methods=['POST'])
 def predict():
+    global predict_status
     if predict_status == "predicting":
         return jsonify({"error": "already predicting"}), 400
-
+    
+    predict_status = "predicting"
     input_file = request.files["input_file"]
 
     model_file = request.form.get("model_file", "model.pkl")
@@ -38,6 +38,7 @@ def predict():
         return jsonify({"error": "Missing required parameters: model_file"}), 400
 
     predict_model(input_file, prediction_file, model_file, clean)
+    predict_status = "not predicting"
 
     return jsonify({
         "Prediction Complete. Saved predictions to ": prediction_file
@@ -45,10 +46,13 @@ def predict():
 
 @app.route("/train", methods=["POST"])
 def train():
+    global train_status
     if train_status == "training":
         return jsonify({"error": "already training"}), 400
-
+    
+    train_status = "training"
     data_file = request.files["data_file"]
+
     model_file = request.form.get("model_file", "model.pkl")
     clean = request.form.get("clean", "true").lower() == "true"
 
@@ -56,8 +60,8 @@ def train():
         return jsonify({"error": "Missing required parameters: data_file"}), 400
 
     train_model(data_file, model_file, clean)
+    train_status = "not training"
 
-    return jsonify({"Training Complete, new model created. Saved to ": model_file,})
-
+    return jsonify({"Training Complete, new model created. Saved to ": model_file})
 if __name__ == "__main__":
     app.run(debug=True)
